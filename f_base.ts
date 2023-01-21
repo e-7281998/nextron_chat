559251c3bdf7b71a8ffe89cf22e658ed22c3bb29
e-7281998 <jejjcs43@naver.com>
2023-01-21 17:29:39 +0900

    firebase 회원가입

---
+++ f_base.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECTE_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,39 +1,89 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import { ipcRenderer } from 'electron';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from '../../f_base';
 
 function Home() {
-  const [msg, setMsg] = useState('');
-  const onChangeMsg = (e) => {
-    setMsg(() => e.target.value);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth(app);
+
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
   }
-  const sendInfo = (e) => {
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const onSignIn = (e) => {
     e.preventDefault();
-
-    //서버로 송신
-    ipcRenderer.send('sendMessage', msg);
-    //서버로부터 수신
-    ipcRenderer.on('replyMessage', (evt, arg) => {
-      console.log(arg);
-      console.log(evt);
-    })
-
-    setMsg('')
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log('성공', userCredential);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('실패');
+        console.log(errorCode, errorMessage);
+        // ..
+      });
   }
 
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-typescript)</title>
+        <title>Nextron Chat</title>
       </Head>
       <div>
         <form>
-          <input type="text" id="textInput" value={msg} onChange={onChangeMsg} />
-          <button onClick={sendInfo} id="btn">전송</button>
+          <input type="email" placeholder='이메일을 입력하세요.' value={email} onChange={onChangeEmail} />
+          <input type="password" placeholder='비밀번호를 입력하세요.' value={password} onChange={onChangePassword} />
+          <button type='submit' onClick={onSignIn}>회원가입</button>
         </form>
       </div>
     </React.Fragment>
   );
 };
+
+
+// function Home() {
+//   const [msg, setMsg] = useState('');
+//   const onChangeMsg = (e) => {
+//     setMsg(() => e.target.value);
+//   }
+//   const sendInfo = (e) => {
+//     e.preventDefault();
+
+//     //서버로 송신
+//     ipcRenderer.send('sendMessage', msg);
+//     //서버로부터 수신
+//     ipcRenderer.on('replyMessage', (evt, arg) => {
+//       console.log(arg);
+//       console.log(evt);
+//     })
+
+//     setMsg('')
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <Head>
+//         <title>Home - Nextron (with-typescript)</title>
+//       </Head>
+//       <div>
+//         <form>
+//           <input type="text" id="textInput" value={msg} onChange={onChangeMsg} />
+//           <button onClick={sendInfo} id="btn">전송</button>
+//         </form>
+//       </div>
+//     </React.Fragment>
+//   );
+// };
 
 export default Home;
